b570aa8dce02e9a1aabd58eaba28ac1a0bc547a3
Noushad Khan <noushadkhan1994@gmail.com>
2019-06-25 23:31:51 +0530

    Update my_dummy_variable.py

--- my_methods/my_dummy_variable.py
+++ my_methods/my_dummy_variable.py
@@ -1,11 +1,16 @@
 class MyDummyVariable:
   #initialize OneHotEncoder for future use when we transform data
   ohe_encoders = {}
-  def __init__(self):
-    pass
+  def __init__(self, drop_first = True):
+    '''it is a class to get dummy variable from a dataframe in this method we are using OneHotEncoder 
+    and this method automatically distinguish numeric and categorical columns'''
+    self.drop_first = drop_first
  
   #fit_transform
   def fit_transform(self, features):
+    '''fit_transform(features)
+    features must be an dataframe
+    it requires an argument features which is a dataframe containing numeric and categorical columns'''
     from sklearn.preprocessing import LabelEncoder, OneHotEncoder
     import numpy as np
     import pandas as pd
@@ -13,7 +18,9 @@ class MyDummyVariable:
     categorical_ohe = None
     for i in categorical_data.columns:
       ohe = OneHotEncoder(handle_unknown = 'ignore', sparse = False)
-      encoded_column = ohe.fit_transform(categorical_data[[i]].values)[:, 1:]
+      encoded_column = ohe.fit_transform(categorical_data[[i]].values)
+      if self.drop_first:
+        encoded_column = encoded_column[:, 1:]
       MyDummyVariable.ohe_encoders[i] = ohe
       if categorical_ohe:
         categorical_ohe = np.concatenate([categorical_ohe, encoded_columns], axis = 1)
@@ -35,13 +42,18 @@ class MyDummyVariable:
   
   #transform data
   def transform(self, features):
+    '''transform(features)
+    features must be an dataframe
+    it requires an argument features which is a dataframe containing numeric and categorical columns'''
     from sklearn.preprocessing import LabelEncoder, OneHotEncoder
     import numpy as np
     import pandas as pd
     categorical_data = features.select_dtypes(include = 'object')
     categorical_ohe = None
     for i in categorical_data.columns:
-      encoded_column = MyDummyVariable.ohe_encoders[i].transform(categorical_data[[i]].values)[:, 1:]
+      encoded_column = MyDummyVariable.ohe_encoders[i].transform(categorical_data[[i]].values)
+      if self.drop_first:
+        encoded_column = encoded_column[:, 1:]
       
       if categorical_ohe:
         categorical_ohe = np.concatenate([categorical_ohe, encoded_columns], axis = 1)
