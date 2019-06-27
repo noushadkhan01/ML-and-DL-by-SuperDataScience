382eb01bf9fc8a587d53cc47fcc95ab0fc1603bd
Noushad Khan <noushadkhan1994@gmail.com>
2019-06-27 14:46:36 +0530

    Update visualise_classifier_performance.py

--- my_methods/visualise_classifier_performance.py
+++ my_methods/visualise_classifier_performance.py
@@ -1,9 +1,20 @@
-def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifier):
+def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifier,important_parameter = None, model_parameters = False):
   #import dependencies
-  from matplotlib.colors import ListedColormap
-  import numpy as np
   import matplotlib.pyplot as plt
-  
+  %matplotlib inline
+  import numpy as np
+  from matplotlib.colors import ListedColormap
+  className = model.__class__.__name__
+  print(f'{className} Model \n')
+  if model_parameters:
+    print(model)
+    print('\n\n')
+  # Visualising the Training set results
+  if important_parameter:
+    imp_feat_value = classifier.__dict__[important_parameter]
+  else:
+    important_parameter = 'not provided'
+    imp_feat_value = ''
   X_set, y_set = X_train, y_train
   X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                        np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
@@ -14,10 +25,10 @@ def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifie
   for i, j in enumerate(np.unique(y_set)):
       plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                   c = ListedColormap(('red', 'green'))(i), label = j)
-  plt.title('Logistic Regression (Training set)')
+  plt.title(f'{className}(important_parameter = {imp_feat_value}) Classifier (Training set)')
   plt.xlabel('Age')
   plt.ylabel('Estimated Salary')
-  plt.legend()
+  plt.legend(loc = 'best', bbox_to_anchor=(1.2, 0.5))
   plt.show()
 
   # Visualising the Test set results
@@ -32,8 +43,8 @@ def visualise_classifier_performance(X_train, X_test, y_train, y_test, classifie
   for i, j in enumerate(np.unique(y_set)):
       plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                   c = ListedColormap(('red', 'green'))(i), label = j)
-  plt.title('Logistic Regression (Test set)')
+  plt.title(f'{className}(important_parameter = {imp_feat_value})Classifier (Test set)')
   plt.xlabel('Age')
   plt.ylabel('Estimated Salary')
-  plt.legend()
+  plt.legend(loc = 'best', bbox_to_anchor=(1.2, 0.5))
   plt.show()
